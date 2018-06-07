39d02412e68000254bacfbcd06304f7fc9965850
wangqiaohqh <39872372+wangqiaohqh@users.noreply.github.com>
2018-06-07 14:34:32 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -7,23 +7,27 @@ This is a temporary script file.
 
 
 
-import json
 import urllib.request as r
-url = 'http://api.openweathermap.org/data/2.5/forecast?q={},cn&mode=json&lang=zh_cn&&APPID=6a67ed641c0fda8b69715c43518b6996&units=metric '
-
-info = r.urlopen(url.format('leshan')).read().decode('utf-8','ignore')
+import json
+city = input("输入城市（拼音）");
+address='http://api.openweathermap.org/data/2.5/forecast?q={},cn&mode=json&lang=zh_cn&&APPID=6a67ed641c0fda8b69715c43518b6996&units=metric' 
+info=r.urlopen(address.format(city)).read().decode('utf-8','ignore') 
 data = json.loads(info)
-print("[0-4]是2018/6/1")
-print("[5-9]是2018/6/2")
-print("[10-14]是2018/6/3")
-print("[15-19]是2018/6/4")
-print("[20-24]是2018/6/5")
-i = input("请输入0-24的数字：")
-tq = {'weather': data['list'][int(i)]['weather'][0]['description'],
-      'temp' : data['list'][int(i)]['main']['temp'],
-      'temp_min' : data['list'][int(i)]['main']['temp_min'],
-      'temp_max' : data['list'][int(i)]['main']['temp_max'],
-      'pressure' : data['list'][int(i)]['main']['pressure'],
-      'day' : data['list'][int(i)]['dt_txt']
-      }
-print("{}的天气是{},最高温度是{},最低温度是{},气压是{}".format(tq['day'],tq['weather'],tq['temp_max'],tq['temp_min'],tq['pressure']))
+date = []
+weather = {"日期":[],"天气":[],"最高气温":[],"最低气温":[],"气压":[]}
+for i in range(len(data["list"])):
+    weather["日期"].append(data["list"][i]["dt_txt"])
+    weather["天气"].append(data["list"][i]["weather"][0]["description"])
+    weather["最高气温"].append(data["list"][i]["main"]["temp_max"])
+    weather["最低气温"].append(data["list"][i]["main"]["temp_min"])
+    weather["气压"].append(data["list"][i]["main"]["pressure"])
+
+string="**********************************\n当前日期：\t{}\n当前天气：\t{}\n最高温度：\t{}\n最低温度：\t{}\n当前气压：\t{}\n\n**********************************"
+pointData = weather["日期"][0][:10]
+print("\n\n\n********************{}********************".format(pointData))
+for i in range(len(data["list"])):
+    if weather["日期"][i][:10] != pointData:
+        pointData = weather["日期"][i][:10]
+        print("\n\n\n********************{}********************".format(pointData))
+    print(string.format(weather["日期"][i],weather["天气"][i],weather["最高气温"][i],weather["最低气温"][i],weather["气压"][i]))
+input("请输入任意字符结束查询：")
